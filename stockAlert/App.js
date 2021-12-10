7957d98390a9f9c5d924979d325c284a95643b90
Rishi Raj <rishisonuraj@gmail.com>
2021-12-10 10:18:56 -0600

    Initial setup

---
+++ stockAlert/App.js
@@ -0,0 +1,15 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import { Text, View } from 'react-native';
+import Tabs from './Tabs';
+
+const app = () => {
+  return (
+    
+
+
+      <Tabs></Tabs>
+
+  )
+}
+export default app;
